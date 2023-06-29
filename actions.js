b73d71935e831ba74a272f639cdfb4f8402e3a42
cane122 <cankovicognjen@gmail.com>
2023-06-29 21:55:27 +0200

    added github activity

--- actions.js
+++ actions.js
@@ -1,22 +1,3 @@
-function checkGuess() {
-    var randomNumber = Math.floor(Math.random() * 100) + 1;
-    var guessInput = document.getElementById("guessInput");
-    var guessResult = document.getElementById("guessResult");
-
-    var userGuess = parseInt(guessInput.value);
-    if (isNaN(userGuess)) {
-        guessResult.textContent = "Invalid input. Please enter a number.";
-    } else {
-        if (userGuess === randomNumber) {
-            guessResult.textContent = "Congratulations! You guessed the correct number.";
-        } else if (userGuess < randomNumber) {
-            guessResult.textContent = "Too low. Try again!";
-        } else {
-            guessResult.textContent = "Too high. Try again!";
-        }
-    }
-}
-
 window.addEventListener('DOMContentLoaded', () => {
     const textContainer = document.getElementById('text-container');
 
