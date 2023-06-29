589002320b09d3403dc755c8518d2e9f99c80501
cane122 <cankovicognjen@gmail.com>
2023-06-29 19:01:38 +0200

    added text and fixed something

--- actions.js
+++ actions.js
@@ -16,3 +16,16 @@ function checkGuess() {
         }
     }
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    const textContainer = document.getElementById('text-container');
+
+    fetch('text.txt')
+        .then(response => response.text())
+        .then(data => {
+            textContainer.textContent = data;
+        })
+        .catch(error => {
+            textContainer.textContent = 'Error: ' + error;
+        });
+});
